a5261fabb4a2e5780004f2d3b27cc2fad6858f3f
iamsajad <s.pr98@yahoo.com>
2023-12-30 01:33:25 +0330

    some configs added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
+import { resolve } from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias:{
+    "@": resolve(__dirname) + '/'
+  },
+  app: {
+    head: {
+      link: [
+        {rel: 'icon' , href: "/MahyaPardazYazd.png"},
+      ]
+    }
+  },
+  css: [
+    "@/assets/global.scss"
+  ]
 })
