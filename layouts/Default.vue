0dd67f1187b53fc6dea58089bfbcf5d8c5e16497
iamsajad <s.pr98@yahoo.com>
2023-12-31 20:46:13 +0330

    default layout added

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,3 +1,10 @@
+<script setup>
+    import Header from '../modules/header/Component.vue'
+    import HeaderBanner from '../modules/header-banner/Component.vue'
+</script>
+
 <template>
+    <Header />
+    <HeaderBanner />
     <slot />
 </template>
