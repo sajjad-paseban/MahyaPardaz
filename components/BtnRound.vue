79821c2e5218f9e2963218465750d2a39fe53e85
iamsajad <s.pr98@yahoo.com>
2023-12-31 20:45:55 +0330

    BtnRound component added

---
+++ components/BtnRound.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const { isSubmit, to, title } = defineProps({ isSubmit: Boolean, to: String, title: String})
+</script>
+
+<template>
+    <NuxtLink v-if="!isSubmit" :to="to">
+        {{ title }}
+    </NuxtLink>
+
+    <button v-else type="submit">
+        {{ title }}
+    </button>
+</template>
+
+<style scoped lang="scss">
+    a, button{
+        font-family: 'yekan';
+        border: 1px solid #ffffff !important;
+        padding: 14px 80px;
+        border-radius: 50px;
+        position: relative;
+        top: 20px;
+        // transition: color,background-color 300ms;
+        &:hover{
+            background-color: #ffffff;
+        }
+    }
+</style>
