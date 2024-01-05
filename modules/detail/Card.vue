44fdde36a790a27ccbaeb6e278d7c85aa7d02cfb
iamsajad <s.pr98@yahoo.com>
2024-01-05 16:34:35 +0330

    detail module added

---
+++ modules/detail/Card.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+    const { number, title } = defineProps({number: String, title: String})
+</script>
+
+<template>
+    <div class="col-auto mx-5 text-center">
+        <div class="line line-top"></div>
+        <h2>{{ number }}</h2>
+        <h3>{{ title }}</h3>
+        <div class="line line-bottom"></div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    h2{
+        font-size: 50px;
+        position: relative;
+        right: 4px;
+    }
+    .line{
+        position: relative;
+        left: calc(50% - 6%);
+        height: 50px;
+        width: 6px;
+        background-color: #fff;
+        &.line-top{
+            border-bottom-left-radius: 5px;
+            border-bottom-right-radius: 5px;
+        }
+        &.line-bottom{
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+
+    }
+    
+    @media screen and (max-width: 500px) {
+        h2{
+            right: 3px;
+        }
+        .line{
+            display: none;
+        }
+    }
+</style>
---
+++ modules/detail/Component.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import DetailCard from '../detail/Card.vue'
+</script>
+
+<template>
+    <div class="detail-section">
+        <div class="row justify-content-center">
+            <DetailCard number="18" title="مشتریان" />
+            <DetailCard number="30" title="اپلیکیشن ها" />
+            <DetailCard number="80" title="کارمندان" />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .detail-section{
+        width: 100%;
+        overflow: hidden;
+        background-image: linear-gradient(127deg, #384DFF 0%, #24CDFF 91%);
+        color: #F7F8FF;
+        font-family: 'yekan';
+    }
+</style>
