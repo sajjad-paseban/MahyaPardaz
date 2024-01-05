b879e7379055ad1f3c604fa2b0ebf09fcb46df6d
iamsajad <s.pr98@yahoo.com>
2024-01-05 14:49:00 +0330

    scripts for page layout added

---
+++ layouts/Page.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Header from '@/modules/header/Component.vue'
+import Footer from '@/modules/footer/Component.vue'
+export default defineComponent({
+    name: 'PageLayout',
+    components: {
+        Header,
+        Footer
+    }
+})
+</script>
+
+<template>
+    <Header />
+    <slot />
+    <Footer />
+</template>
