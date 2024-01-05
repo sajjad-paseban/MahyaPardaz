16c85c20b0f70083b6d1fa4f6504debef451b9da
iamsajad <s.pr98@yahoo.com>
2024-01-05 14:48:23 +0330

    changes on default layout applied

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,10 +1,27 @@
 <script setup>
     import Header from '../modules/header/Component.vue'
+    import Footer from '../modules/footer/Component.vue'
     import HeaderBanner from '../modules/header-banner/Component.vue'
 </script>
 
 <template>
     <Header />
     <HeaderBanner />
-    <slot />
+    <section class="main-section">
+        <slot /> 
+    </section>
+    <footer class="footer-section">
+        <Footer />
+    </footer>
 </template>
+
+<style lang="scss" scoped>
+    .main-section{
+        position: relative;
+        top: 640px;
+    }
+    .footer-section{
+        position: relative;
+        top: 640px;
+    }
+</style>
