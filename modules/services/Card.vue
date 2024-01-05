52ae91bdf64050fe5801f0ea96bef5eb64355179
iamsajad <s.pr98@yahoo.com>
2024-01-05 16:32:58 +0330

    service module added

---
+++ modules/services/Card.vue
@@ -0,0 +1,60 @@
+<script>
+    export default defineComponent({
+        name: 'ServiceCard',
+        props: {
+            isActive:{
+                type: Boolean,
+                default: false
+            },
+            title: {
+                type: String,
+            },
+            description: {
+                type: String,
+            }
+        }
+    })
+</script>
+
+<template>
+    <div class="item p-4" :class="{active: isActive}">
+        <h4>
+            {{ title }}
+        </h4>
+        <p>
+            {{ description }}
+        </p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .item{
+        background-color: linear-gradient(127deg, #fff 0%, #fff 91%);
+        box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.06);
+        border-radius: 40px 40px 4px 40px;
+        width: calc(40.5% - 20px);
+        margin: 10px;
+        direction: rtl;
+        font-family: 'yekan';
+        &.active{
+            background-image: linear-gradient(127deg, #384DFF 0%, #24CDFF 91%);
+            color: #F7F8FF;
+            p{
+                color: #F7F8FF;
+            }
+        }
+        
+        p{
+            font-size: 18px;
+            line-height: 1.5;
+            color: #6F8BA4;
+        }
+    }
+   
+    @media screen and (max-width: 550px) {
+        .item{
+            margin: 10px 30px !important;
+            width: calc(100% - 100px) !important;
+        }
+    }
+</style>
---
+++ modules/services/Component.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="services-section py-5">
+        <div class="row justify-content-center flex-column-reverse flex-lg-row">
+            <div class="col-12 col-lg-6 items d-flex flex-wrap justify-content-center justify-content-lg-end">
+                <slot />
+            </div>
+            <div class="col-12 col-lg-6 intro justify-content-center align-self-center">
+                <h2>
+                    اپلیکیشن موبایل یک ابزار تخصصی کسب و کار آنلاین است.
+                </h2>                
+                <p>
+                    طراحی اپلکیشن موبایل معمولا به چند انگیزه انجام می شود، بسته به این انگیزه ها، توسعه اپلیکیشن موبایل تعاریف متفاوتی می پذیرد.
+
+توسعه اپلیکیشن موبایل کاری بسیار تخصصی است. توسعه اپلیکیشن نیازمند دانش فنی، شناخت مارکت های اپلیکیشن (گوگل پلی، اپ استور، کافه بازار، سیبچه و…)، شناخت مخاطبین اپلیکیشن، مدیریت پروژه اپلیکیشن، مدیریت برند اپلیکیشن و… است.
+                </p>
+
+                <BtnRound to="/service" title="خدمات" :isPrimary="true" />
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    .services-section{
+        width: 100%;
+        overflow: hidden;
+        color: #3B566E;
+        background-color: rgba(255, 255, 238, 0.3);
+        .items{
+            padding-left: 240px;
+        }
+
+        .intro{
+            direction: rtl;
+            padding-right: 15%;
+            padding-left: 5%;
+            font-family: 'yekan';            
+            line-height: 1.5;
+            p{
+                font-size: 18px;
+                text-align: justify;
+            }
+        }
+    }
+    
+    @media screen and (max-width: 950px) {
+        .intro{
+            padding: 10% !important;
+        }
+        .items{
+            padding-left: 0 !important;
+        }
+    }
+</style>
