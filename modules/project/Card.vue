88511ba755c9a34d2ebd1783612bdcc9857f906d
iamsajad <s.pr98@yahoo.com>
2024-01-05 16:35:20 +0330

    project module added

---
+++ modules/project/Card.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+    const {title, imgSrc, description, to} = defineProps({title: String, imgSrc: String, description: String, to: String}) 
+</script>
+
+<template>
+    <div class="col-auto item bg-white px-4 py-5 m-2">
+        <img :src="imgSrc" alt="">
+        <h3 class="text-center">
+            {{ title }}
+        </h3>
+        <p>
+            {{ description }}
+        </p>
+        <btn-round :is-block="true" title="جزئیات اپلیکیشن" :to="to" :is-primary="true" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .item{
+        overflow: hidden;
+        border-radius: 15px;
+        box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.06);
+        // width: 350px;
+        img{
+            width: 350px;
+            min-height: 200px;
+            object-position: center center;
+            object-fit: cover;
+        }
+
+        p{
+            direction: rtl;
+            width: 350px;
+            text-align: justify;
+        }
+
+    }
+</style>
---
+++ modules/project/Component.vue
@@ -0,0 +1,52 @@
+<script>
+    export default defineComponent({
+        name: "ProjectModule",
+        props: {
+            showAllBtn: {
+                type: Boolean,
+                default: true
+            },
+            showHeader: {
+                type: Boolean,
+                default: true
+            }
+        }
+    })    
+</script>
+
+<template>
+    <div class="project-section py-5">
+        <div class="row" v-if="showHeader">
+            <div class="col text-center">
+                <h2>
+                    پروژه های ما
+                </h2>
+            </div>
+        </div>
+        <div class="row" v-if="showHeader">
+            <div class="col text-center">
+                <p class="px-5">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است                    
+                </p>
+            </div>
+        </div>
+        <div class="row justify-content-center my-3">
+            <slot />
+        </div>
+        <div class="row justify-content-center my-5" v-if="showAllBtn">
+            <div class="col-auto">
+                <btn-round to="/project" title="مشاهده همه پروژه" :is-primary="true" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .project-section{
+        background-color: rgba(244, 248, 251, 0.8);
+        font-family: 'yekan';
+        width: 100%;
+        overflow: hidden;
+        color: #3B566E;
+    }
+</style>
