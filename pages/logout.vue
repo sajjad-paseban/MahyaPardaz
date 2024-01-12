01cca72636b567e1a33b172105c106704908099d
iamsajad <s.pr98@yahoo.com>
2024-01-13 02:19:38 +0330

    logout completed

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,3 +1,15 @@
+<script>
+export default defineComponent({
+    name: 'LogoutPage',
+    beforeCreate(){
+        console.log(1)
+        localStorage.removeItem('SESS_COK')
+        navigateTo('/')
+    }
+
+})
+</script>
+
 <template>
     
 </template>
